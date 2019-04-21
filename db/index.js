76b821175bb4d1a4117dfad8c281435c707aa4e1
Vlad <vladislav.kuzmich.96@mail.ru>
2019-04-22 01:24:07 +0300

    отказался от идеи с перехватчиками, доработал передачу данных

--- db/index.js
+++ db/index.js
@@ -1,22 +1,25 @@
 let array = [];
 let flag=0;
 class User{
-    constructor(name, email, password){
-        this.userName=name;
+    constructor(name, email, password, phone, adres){
+        this.name=name;
         this.email = email;
         this.password= password;
+        this.phone =phone;
+        this.adres =adres;
     }
 }
 
-exports.put = function(name, email, password){
+exports.put = function(name, email, password, phone, adres){
     array.push(new User(name, email, password));
     flag++
 };
 
 exports.equals = function (email, password) {
     for( i =0; i<flag; i++){
-        if(array[i].email === email&&array[i].password===password)
+        if(array[i].email === email&&array[i].password===password) {
             return true;
+        }
     }
     return false;
 };
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const Cookies = require('cookies');
 const db = require ('./db');
 const app = express();
 
@@ -9,33 +8,22 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use("/update", (req, res, next) => {
-    if((new Cookies(req, res)).get('Access') === true){
-        next();
-    }
-    else {
-        res.send('1');
-    }
-});
+
 
 app.get("/registration", (req,resp) =>{
-    db.put(req.query.name, req.query.email, req.query.password);
-    resp.send("1");
+    db.put(req.query.name, req.query.email, req.query.password, null, null);
+    resp.send(true);
 });
 
 app.get("/enter", (req,resp) =>{
-    if('enter ' + req.query.email + ' ' + req.query.password){
-        let CookieOptions ={
-            path:'/'
-        };
-        (new Cookies(req, resp)).set('Access',true, CookieOptions)
-        resp.send('1')
+    if(db.equals(req.query.email, req.query.password)){
+        resp.send(true)
+    }else {
+        resp.send(false);
     }
-    resp.send("2");
 });
 
 app.get("/update", (req, resp) =>{
-    console.log("user was here!!")
 });
 
 app.listen(8080, ()=>
